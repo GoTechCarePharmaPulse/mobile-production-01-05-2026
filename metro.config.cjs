65bf164acd8a15bf6ab3bb2c322a43434aae6c61
Shrawan Kumar <support@gotechcare.com>
2026-06-14 08:39:28 +0530

    fix error  code 500

--- metro.config.cjs
+++ metro.config.cjs
@@ -6,6 +6,7 @@ const config = getDefaultConfig(__dirname);
 // ✅ Prevent Razorpay crash on web
 config.resolver.extraNodeModules = {
   "react-native-razorpay": path.resolve(__dirname, "emptyModule.js"),
+  "buffer": require.resolve("buffer")
 };
 
 module.exports = config;
